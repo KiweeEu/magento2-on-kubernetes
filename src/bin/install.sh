435e76a09546c3696f3fac1be979c09b0b0eb612
Maciej Lewkowicz <maciej.lewkowicz@kiwee.eu>
2023-05-31 14:40:51 +0200

    Fix install script

--- src/bin/install.sh
+++ src/bin/install.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
-trap exit ERR
+set -xe
 
-while ! nc -z $DB_HOST 3306;
-do
-  echo "Waiting for database...";
-  sleep 1;
-done;
-echo "Database found";
+export INSTALL_MAGENTO=1
 
-php bin/magento setup:config:set \
-    --no-interaction \
-    --skip-db-validation \
-    --db-host=$DB_HOST \
-    --db-name=$DB_NAME \
-    --db-user=$DB_USER \
-    --db-password=$DB_PASS \
-    --backend-frontname=$ADMIN_URI \
-    --key=$KEY
+# https://github.com/magento/magento2/issues/34566
+mv app/etc/config.php app/etc/config.php.bak
 
 yes | php bin/magento setup:install \
+    --no-interaction \
+    --skip-db-validation \
+    --backend-frontname=$ADMIN_URI \
+    --key=$KEY \
     --admin-user=$ADMIN_USER \
     --admin-password=$ADMIN_PASSWORD \
     --admin-firstname=$ADMIN_FIRSTNAME \
     --admin-lastname=$ADMIN_LASTNAME \
     --admin-email=$ADMIN_EMAIL
+
+mv app/etc/config.php.bak app/etc/config.php
+
+php bin/magento setup:db:status || php bin/magento setup:upgrade --keep-generated
+php bin/magento app:config:status || php bin/magento app:config:import
