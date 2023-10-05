57f7cf3afb71dcbbae09c4cf4abe5637097eddc7
Maciej Lewkowicz <maciej.lewkowicz@kiwee.eu>
2023-10-05 10:53:29 +0200

    Update Cypress config to v13

---
+++ test/e2e/cypress.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  defaultCommandTimeout: 30000,
+  requestTimeout: 30000,
+  pageLoadTimeout: 30000,
+  video: false,
+  viewportWidth: 1280,
+  viewportHeight: 800,
+  retries: {
+    runMode: 0,
+    openMode: 0,
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl:
+      'http://ingress-nginx-nginx-ingress-controller.default.svc.cluster.local/',
+    specPattern: 'cypress/e2e/**/*.{spec.js,feature}',
+  },
+})
